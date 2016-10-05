11f9357eea569a1f531985aa22e480743da0ace3
jaj <jona@joachim.cc>
2016-10-05 17:03:36 +0200

    Add window quantile

--- winquant.m
+++ winquant.m
@@ -1,15 +1,19 @@
-function [ res ] = winsum( rwin )
-%WINSUM Summary of this function goes here
+function [ res ] = winquant( rwin, quant, varargin )
+%WINQUANT Summary of this function goes here
 %   Detailed explanation goes here
     shape = size(rwin);
     if length(shape) > 2
         error('Incorrect window');
     end
     
+    if nargin < 2
+        quant = .7;
+    end
+    
     vecsize = shape(2);
     res = zeros(1, vecsize);
-    
+
     for i = 1:vecsize
-        res(:,i) = sum(rwin(:,i));
+        res(:,i) = quantile(rwin(:,i), quant);
     end
 end
