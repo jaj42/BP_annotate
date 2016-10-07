f765bb9d5aeadec76f19acdea6de157535313679
jaj <jona@joachim.cc>
2016-10-07 11:12:00 +0200

    Add code for ZOI identification. Not correct yet because we need a centered window.

--- BP_annotate.m
+++ BP_annotate.m
@@ -9,19 +9,38 @@ function [ footIndex, systolicIndex, notchIndex ] = BP_annotate( waveform, fs, v
     
     [ waveformDDPlus, newFs ] = doubleDerive( waveform, fs );
     
+    threswinsize = floor(newFs * 3);
+    integwinsize = floor(newFs / 4);
 
+    integwindow = window(waveformDDPlus, integwinsize);
+    integral = winsum(integwindow);
 
+    threswindow = window(integral, threswinsize);
+    threshold = winquant(threswindow, .7);
+    
+    zois = integral > threshold;
+
+    
     if verbose
         time = (0: length(waveform) - 1) ./ fs;
         newTime = (0: length(waveformDDPlus) - 1) ./ newFs;
         figure
-        axs(1) = subplot(3, 1, 1);
+        axs(1) = subplot(4, 1, 1);
         plot(time, waveform);
         
-        axs(2) = subplot(3, 1, 2);
-        plot(newTime, waveformDDPlus)
+        axs(2) = subplot(4, 1, 2);
+        plot(newTime, waveformDDPlus);
+        
+        axs(3) = subplot(4, 1, 3);
+        plot(newTime, integral);
+        hold on;
+        plot(newTime, threshold);
+        legend('Integral', 'Threshold');
+
+        axs(4) = subplot(4, 1, 4);
+        plot(newTime, zois);
+        legend('ZOIs');
         
         linkaxes(axs, 'x')
     end
 end
-
