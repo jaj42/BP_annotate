393911b3d3e439dee1b476854bde68c1e259f22f
Alexandre Laurin <alexandre.laurin@inria.fr>
2016-10-07 14:34:36 +0200

    "Fixed, start and end ."

--- BP_downsample.m
+++ BP_downsample.m
@@ -6,15 +6,15 @@ function [ newWaveform, newFs ] = BP_downsample( waveform, fs )
 if fs > 200
     oldTime = (0: length(waveform) -1) ./ fs;
     newTime = (oldTime(1): 1/200: oldTime(end));
-    newWaveform = interp1(oldTime, waveform, newTime, 'pchip');
+    newWaveform = interp1(oldTime, waveform, newTime, 'pchip');%Interpolation for downsampling
     newWaveform = newWaveform(:);%Column
     
-    %Low-pass filter to remove noise
+    %Low-pass filter to remove noise (assumes fs = 200)
     b = [1 0 0 0 0 0 -2 0 0 0 0 0 1];
     a = [1 -2 1];
     h_l = filter(b,a,[1 zeros(1,12)]); 
     newWaveform = conv (newWaveform ,h_l);
-
+    newWaveform = [newWaveform(13).*ones(7,1); newWaveform(13:end-13); newWaveform(end-13).*ones(20, 1)];
     newFs = 200;
 else
     newWaveform = waveform;
