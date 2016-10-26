d4d8fad86fb25fa30a64eecded5a8e62789c60e3
jaj <jona@joachim.cc>
2016-10-26 11:20:15 +0200

    New BP_resample function to convert to 200 Hz.

---
+++ BP_resample.m
@@ -0,0 +1,10 @@
+function [ newWaveform, newx, oldx ] = BP_resample( waveform, fs )
+%BP_RESAMPLE Resample to 200 Hz
+newfs = 200;
+
+duration = length(waveform) / fs;
+
+oldx = linspace(0, duration, length(waveform));
+newx = linspace(0, duration, newfs * duration);
+
+newWaveform = interp1(oldx, waveform, newx);
