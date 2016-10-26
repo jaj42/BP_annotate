2e0e1274b2c8b085bdb28c1380cc54576a0ac3f1
jaj <jona@joachim.cc>
2016-10-26 11:55:48 +0200

    Adapt doublederive to the new lowpass / resample functions.

--- doubleDerive.m
+++ doubleDerive.m
@@ -1,37 +1,22 @@
-function [ waveformDDPlus, waveformDD ] = doubleDerive( waveform, fs )
-%[ waveformDD ] = doubleDerive( waveform, fs )
-%
+function [ waveformDDPlus, waveformDD ] = doubleDerive( waveform )
 %Returns the second derivative of the waveform if the first derivative is
 %positive; otherwise returns
-%%
-    %Perform the first derivative
-    waveformD = diff(waveform);
-    waveformD = waveformD .* fs;
-    waveformD = [waveformD(1); waveformD];
-    
-    %Perform the second derivative
-    waveformDD = diff(waveformD);
-    waveformDD = waveformDD.*fs;
-    waveformDD = [0; waveformDD];
-    
-    if fs < 200
-        oldTime = (0: length(waveform) - 1) ./ fs;
-        newTime = (0: 1/200: oldTime(end));
-        tempWaveformDD = interp1(oldTime, waveformDD, newTime, 'pchip');
-    else
-        tempWaveformDD = waveformDD;
-    end
-     %Low-pass filter to remove noise (assumes fs = 200)
-    b = [1 0 0 0 0 0 -2 0 0 0 0 0 1];
-    a = [1 -2 1];
-    h_l = filter(b,a,[1 zeros(1,12)]); 
-    waveformDD = conv (tempWaveformDD ,h_l);
-%     waveformDD = [waveformDD(13).*ones(7,1); waveformDD(14:end-13); waveformDD(end-13).*ones(19, 1)];
-    waveformDD = waveformDD(13:end);
-    
-    %Perform the switch for positive and negative first-derivative values
-    waveformDDPlus = zeros( size(waveformDD) );
-    waveformDDPlus(waveformD > 0 & waveformDD > 0) = waveformDD(waveformD > 0 & waveformDD > 0);
-    waveformDDPlus = waveformDDPlus .^ 2;
-    waveformDDPlus(1: 2) = 0;
-end
+
+%Perform the first derivative
+waveformD = diff(waveform);
+waveformD = [waveformD(1), waveformD];
+
+%Perform the second derivative
+waveformDD = diff(waveformD);
+waveformDD = [0, waveformDD];
+
+%Low-pass filter to remove noise (assumes fs = 200)
+waveformD  = BP_Lowpass(waveformD);
+waveformDD = BP_Lowpass(waveformDD);
+
+%Perform the switch for positive and negative first-derivative values
+% XXX
+waveformDDPlus = zeros( size(waveformDD) );
+waveformDDPlus(waveformD > 0 & waveformDD > 0) = waveformDD(waveformD > 0 & waveformDD > 0);
+waveformDDPlus = waveformDDPlus .^ 2;
+waveformDDPlus(1: 2) = 0;
